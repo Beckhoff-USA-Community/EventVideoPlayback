e19787961748a4141aae8185d07053a4cfc9dd67
DT <D.thompson@beckhoff.com>
2022-01-24 17:06:33 -0700

    Updated PLC and Batch file to create video with passing file paths. Now working.

--- VideoPlaybackSupportFiles/Images2Video.bat
+++ VideoPlaybackSupportFiles/Images2Video.bat
@@ -1,30 +1,40 @@
+rem Commandline test string = Images2Video Nathan "C:\Temp\Scope Project_Img_test\\" "C:\Temp\Scope Project_Img_test\<Export #>\ipImageResultDisp\"
+rem - example export1 == Images2Video Nathan "C:\Temp\Scope Project_Img_test\\" "C:\Temp\Scope Project_Img_test\Export 1\ipImageResultDisp\"
 
 set videofilename=%1
 set videodestinationpath=%2
 set scopeimageexportpath=%3
 
+echo %videofilename%
+echo %videodestinationpath%
+echo %scopeimageexportpath%
+
+
 rem for /r %%i in (*.jpg) do (
-	rem ffmpeg -framerate 10 -i %%i.jpg -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
+    rem ffmpeg -framerate 10 -i %%i.jpg -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
 rem )
 
 echo off
 color 3
-set a="*.jpg"
-set b=%videofilename%".mp4"
-set c=ffmpeg
-set f=-c:v libx264 -r 60 -framerate 10 -pix_fmt yuvj420p -framerate 10
 
+rem //added the arguments to the two vars below
+set a=%scopeimageexportpath:~1,-1%
+set a="%a%*.jpg"
+set b=%videodestinationpath:~1,-1%%videofilename:~1,-1%
+set b="%b%.mp4"
+set bootfolder=C:\TwinCAT\3.1\boot\
+
+set c="%bootfolder%ffmpeg"
+set f=-c:v libx264 -r 60 -framerate 10 -pix_fmt yuvj420p -framerate 10
 rem (was at end of previous line) -crf 
 
+set tmp="%TEMP%\list.tmp"
 
-set tmp="list.tmp"
+rem pause
 
-del %b%
 
-for %%f in (%a%) do (@echo file 'file:%cd%\%%f' >> %tmp%)
+for %%f in (%a%) do (@echo file 'file:%%f' >> %tmp%)
 %c% -y -f concat -safe 0 -i %tmp% -framerate 10 %f% %b%
+del /f /q %tmp%
 
-
-rem cd /d Result
-del /f /q list.tmp
-rem exit
+exit
